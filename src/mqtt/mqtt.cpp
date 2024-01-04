fddf902f94a1aa2b569a2992ac2e5cc069dfd946
J-Rios <jrios.github@gmail.com>
2024-01-04 16:36:44 +0100

    Added missing file headers information

--- src/mqtt/mqtt.cpp
+++ src/mqtt/mqtt.cpp
@@ -1,3 +1,51 @@
+/**
+ * @file    mqtt.cpp
+ * @author  Jose Miguel Rios Rubio <jrios.github@gmail.com>
+ * @date    2024-01-03
+ * @version 1.0.0
+ *
+ * @section DESCRIPTION
+ *
+ * ESPMULTILOG MQTT Communication source file.
+ *
+ * Check Messages:
+ * mosquitto_sub -F '%I\n%t\n%p\n' -h "test.mosquitto.org" -p 1883
+ *               -t "/24:0A:C4:81:AD:F8/control/+"
+ *
+ * Request FW Version:
+ * mosquitto_pub -h "test.mosquitto.org" -p 1883
+ *               -t "/24:0A:C4:81:AD:F8/control/in" -m "version"
+ *
+ * Request Reboot:
+ * mosquitto_pub -h "test.mosquitto.org" -p 1883
+ *               -t "/24:0A:C4:81:AD:F8/control/in" -m "reboot"
+ *
+ * @section LICENSE
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Jose Miguel Rios Rubio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*****************************************************************************/
 
 /* Libraries */
 
--- src/mqtt/mqtt.h
+++ src/mqtt/mqtt.h
@@ -1,16 +1,36 @@
-
 /**
- * Check Messages:
- * mosquitto_sub -F '%I\n%t\n%p\n' -h "test.mosquitto.org" -p 1883
- *               -t "/24:0A:C4:81:AD:F8/control/+"
+ * @file    mqtt.h
+ * @author  Jose Miguel Rios Rubio <jrios.github@gmail.com>
+ * @date    2024-01-03
+ * @version 1.0.0
  *
- * Request FW Version:
- * mosquitto_pub -h "test.mosquitto.org" -p 1883
- *               -t "/24:0A:C4:81:AD:F8/control/in" -m "version"
+ * @section DESCRIPTION
  *
- * Request Reboot:
- * mosquitto_pub -h "test.mosquitto.org" -p 1883
- *               -t "/24:0A:C4:81:AD:F8/control/in" -m "reboot"
+ * ESPMULTILOG MQTT Communication header file.
+ *
+ * @section LICENSE
+ *
+ * MIT License
+ *
+ * Copyright (c) 2024 Jose Miguel Rios Rubio
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
  */
 
 /*****************************************************************************/
